a1852bcfed09c891a2b2e2c28f99e57012743ff7
vetleat <vetleat99@gmail.com>
2020-05-07 00:05:56 +0200

    Fikset CSS+lagd mapper
    sry er litt OCD med å sortere i mapper så det ser clean ut <3

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Kode for hamburgermeny
+
+function BurgerMenu() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+};
