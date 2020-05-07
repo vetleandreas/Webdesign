b65b753693ab1144526fa25d64af0863bff13a90
vetleat <vetleat99@gmail.com>
2020-05-07 15:49:02 +0200

    Slideshow + nye SoMe buttons

--- js/index.js
+++ js/index.js
@@ -8,3 +8,48 @@ function BurgerMenu() {
     x.style.display = "block";
   }
 };
+
+// Kode for slideshow
+/*
+let slideIndex = 0;
+karusell();
+
+function karusell() {
+  let i;
+  let x = document.getElementsByClassName("Slideshow");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(karusell, 6000); // Endrer bilde hvert 10. sekund
+}; */
+// Kode for knapper på slideshow
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("Slideshow");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-white";
+
+};
