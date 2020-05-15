e5b5d7c3932e0cb16aa3163d38bfceb3dd884314
vetleat <55831510+vetleat@users.noreply.github.com>
2020-05-15 23:52:37 +0200

    Final version

--- js/index.js
+++ js/index.js
@@ -22,32 +22,58 @@ function BurgerMenu() {
     z.style.background == 'url("./img/bk.png") center center / 75% no-repeat'
   ) {
     z.style.background = 'url("./img/bkto.png") no-repeat center';
-    document.getElementById("navbar").style.height = "70vh";
+    document.getElementById("navbar").style.height = "85vh";
     document.getElementById("navbar").style.backgroundColor = "#000000";
     document.getElementById("navbarhide").style.display = "none";
-    document.getElementById("socialmedia").style.position = "absolute";
+    document.getElementById("socialmedia").style.position = "static";
+    document.getElementById("footerline").style.position = "static";
     document.getElementById("socialmedia").style.bottom = "3rem";
-    document.getElementById("footer").style.height = "30vh";
+    document.getElementById("footer").style.height = "15vh";
     document.getElementById("footer").style.backgroundImage =
       'url("https://www.transparenttextures.com/patterns/mirrored-squares.png")';
     document.getElementById("footer").style.backgroundColor = "#000000";
-    document.getElementById("footerline").style.backgroundColor = "#fff766";
+    document.getElementById("footerline").style.backgroundColor = "#ffd23f";
     document.getElementById("buttonsnavbar").style.marginTop = "20%";
+    checkFooter();
   } else {
     z.style.background = 'url("./img/bk.png") no-repeat center';
     document.getElementById("navbar").style.height = "auto";
     document.getElementById("navbar").style.backgroundColor = "#6969b3";
     document.getElementById("navbarhide").style.display = "block";
     document.getElementById("footer").style.height = "";
-    document.getElementById("footer").style.backgroundColor = "#ffffff";
+    document.getElementById("footer").style.backgroundColor = "#f4fdf1";
     document.getElementById("buttonsnavbar").style.marginTop = "2%";
     document.getElementById("footerline").style.backgroundColor = "#6969b3";
+    document.getElementById("socialmedia").style.position = "static";
+    document.getElementById("footerline").style.position = "static";
+    checkFooter();
   }
 
   if (z.style.backgroundSize === "75%") {
     z.style.backgroundSize = "75%";
   } else {
     z.style.backgroundSize = "75%";
+  }
+}
+
+function checkFooter() {
+  console.log(document.getElementById("burgermeny").style.background);
+  let elements = document.getElementsByClassName("mediapic");
+  for (let i = 0; i < elements.length; i++) {
+    let tempString = elements[i].src;
+    let filepath = tempString.split("/");
+    if (filepath[filepath.length - 1].charAt(0) != "p") {
+      let lastsubstr = "img/p" + filepath[filepath.length - 1];
+      console.log(lastsubstr);
+      elements[i].src = lastsubstr;
+    } else if (
+      document.getElementById("burgermeny").style.background ==
+      'url("./img/bkto.png") center center no-repeat'
+    ) {
+      let lastsubstr = "img/" + filepath[filepath.length - 1].substr(1);
+      console.log(lastsubstr);
+      elements[i].src = lastsubstr;
+    }
   }
 }
 
